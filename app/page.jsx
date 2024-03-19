0be4823f7280225a219b8260a90e35abf5c9b5ee
Srinath10X <srinathcreates@gmail.com>
2024-03-19 22:52:17 +0530

    Configured Header hamburger menu new ui

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Header from "@/Components/Header";
 import Main from "@/Components/Main";
+import Showcase from "@/Components/Showcase";
 
 const page = () => {
   return (
     <>
       <Header />
       <Main />
+      <Showcase />
     </>
   );
 };
